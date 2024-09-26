327b508299f0c33207b758f4d72d88aa31694919
eedo_y <doup2001@naver.com>
2024-09-26 11:27:19 +0900

    feat : 로그인 페이지 & 컴포넌트 & 라우터 설정 ✅

---
+++ src/pages/member/LogoutPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import LogoutComponent from '../../components/member/LogoutComponent';
+import BasicLayout from '../../layouts/Basic Layout';
+
+function LogoutPage() {
+  return (
+    <div>
+      <BasicLayout>
+        <LogoutComponent />
+      </BasicLayout>
+    </div>
+  );
+}
+
+export default LogoutPage;
--- src/rootRouter/MemberRouter.js
+++ src/rootRouter/MemberRouter.js
@@ -2,6 +2,7 @@ import React, { lazy, Suspense } from 'react';
 
 const Loading = <div>Loading...</div>;
 const Login = lazy(() => import('../pages/member/LoginPage'));
+const Logout = lazy(() => import('../pages/member/LogoutPage'));
 
 const MemberRouter = () => {
   return [
@@ -10,6 +11,14 @@ const MemberRouter = () => {
       element: (
         <Suspense fallback={Loading}>
           <Login />
+        </Suspense>
+      ),
+    },
+    {
+      path: 'logout',
+      element: (
+        <Suspense fallback={Loading}>
+          <Logout />
         </Suspense>
       ),
     },
